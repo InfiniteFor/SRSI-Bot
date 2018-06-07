dd7216ff672bcef1996d6df2d1e8e161f2fb039a
InfiniteFor <wesleyvdkamp@gmail.com>
2018-06-07 15:23:54 +0200

    Update notify.py

--- notify.py
+++ notify.py
@@ -1,9 +1,8 @@
 import logging
 import telegram
 
-#chatid  = 416075698 #wes
-chatid  = 372188992 #york
 key     = 'PAST_API_KEY_HERE'
+chatid  = 372188992
 
 def notify_log(msg):
     logging.basicConfig(filename='SRSI_bot.log',format='%(asctime)s: %(message)s', datefmt='%d/%m/%Y %I:%M:%S %p')
